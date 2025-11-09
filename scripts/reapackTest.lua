fc188d2a3a57ddae921a26a847a09733cba45729
sinfricia <you@example.com>
2025-11-09 12:59:02 +0100

    Added ReaPack header to script

--- scripts/reapackTest.lua
+++ scripts/reapackTest.lua
@@ -1,1 +1,10 @@
+-- @description My Awesome Script
+-- @version 1.0.0
+-- @author Your Name
+-- @changelog
+--   Initial release
+-- @about
+--   A short description of what this script does.
+
+
 reaper.ShowConsoleMsg("Hello World")
